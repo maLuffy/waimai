96814b05dc09c05d3b97bef286b626dc092bbcd5
maxiaolei <2623758737@qq.com>
2018-03-03 13:47:37 +0800

    添加database.php

---
+++ application/database.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // 服务器地址
+'hostname'       => '127.0.0.1',
+
+// 数据库名
+'database'       => 'waimai',
+
+// 用户名
+'username'       => 'waimai',
+
+// 密码
+'password'       => 'ZptYBtCHdc',
+
+// 数据库编码默认采用utf8
+'charset'        => 'utf8',
+
+// 数据库表前缀
+'prefix'         => 'ruit_',
+
+
+];
